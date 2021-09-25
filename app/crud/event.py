2e46b61052164b830edd6a513358ca9a756bd99e
Ilushkins <46647517+Sn0wy-0wl@users.noreply.github.com>
2021-09-25 21:38:07 +0300

    Added models

--- app/crud/event.py
+++ app/crud/event.py
@@ -6,26 +6,31 @@ from ..database.mongodb import AsyncIOMotorClient
 from bson.objectid import ObjectId
 from ..database.mongodb import AsyncIOMotorClient
 from ..core.config import database_name, events_collection_name, events_images_collection_name
-from ..models.event import Event, EventRequestDto
+from ..models.event import Event, EventImage, EventRequestDto
 from gridfs import GridFS
 from PIL import Image 
 
 async def create_event(conn: AsyncIOMotorClient, event: Event, image: UploadFile) -> Event:
     db = conn[database_name]
-    print(image.file.readlines())
-    result = await db[events_collection_name].insert_one(event.dict())
+    result = await db[events_collection_name].insert_one(event.dict()) 
+    print(str(image.file.readlines()))
+    print(str(result.inserted_id))
+    img = EventImage(event_id=str(result.inserted_id), image_bytes=str(image.file.read()))
+    await db[events_images_collection_name].insert_one(img.dict()) 
     return event
     
 async def get_event_by_id(conn: AsyncIOMotorClient, event_id: str) -> EventRequestDto:
-    event = await conn[database_name][events_collection_name].find_one({'event_id': ObjectId(event_id)})
-    
+    event = await conn[database_name][events_collection_name].find_one({'_id': ObjectId(event_id)})
+    event_image = await conn[database_name][events_images_collection_name].find_one({'event_id': ObjectId(event_id)})
+
     if event:
-        return Event(**event)
+        return EventRequestDto(**event, **event_image)
 
     raise EntityDoesNotExist()
 
 async def get_events(conn: AsyncIOMotorDatabase, from_id: str, ammount: int) -> List[EventRequestDto]:
     event = await conn[database_name][events_collection_name].find().sort({"event_id":from_id}).limit(ammount)
+    
     
     if event:
         return List(**result)
